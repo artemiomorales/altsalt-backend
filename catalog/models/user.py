2ebb2d18edec7cbb7f28d3fcf172a9464b4134bc
Ricardo <ric.morales22@gmail.com>
2020-07-31 14:54:31 -0400

    Allow profileImage field on user to be null to fix Graphql issue

--- catalog/models/user.py
+++ catalog/models/user.py
@@ -15,7 +15,7 @@ class User(AbstractUser):
     last_name = models.CharField(_('last name'), max_length=150, blank=False)
     display_name = models.CharField(max_length=150, blank=True)
     short_name = models.CharField(max_length=18, blank=True)
-    profile_image = models.ImageField(upload_to=profile_image_path, blank=True)
+    profile_image = models.ImageField(upload_to=profile_image_path, null=True, blank=True)
     description = models.TextField()
     location = models.CharField(max_length=50, blank=True)
     pronouns = models.CharField(max_length=50, blank=True)
