eb13b7feda53aa65c0f22eb2fa8b710ddf8ff738
Ricardo <ric.morales22@gmail.com>
2020-09-29 00:27:18 -0400

    Simplify CSRF view so it doesn't return extraneous JSON

--- catalog/views.py
+++ catalog/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.middleware.csrf import get_token
 
 
 def csrf(request):
     csrf_data = get_token(request)
-    response = JsonResponse({'csrfToken': csrf_data})
-    # JsonResponse({'csrfToken': get_token(request)})
+    response = HttpResponse()
     response.set_cookie('csrftoken', csrf_data, max_age=None, expires=None, path='/',
                         domain='altsalt-local.com', secure=True, httponly=False, samesite=None)
     response.cookies['csrftoken']['samesite'] = 'None'
