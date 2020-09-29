6cb2a79fd30317cf0b060a30c811843e110180e5
Ricardo <ric.morales22@gmail.com>
2020-09-28 21:34:02 -0400

    Enable CSRF protection

--- altsalt_backend/urls.py
+++ altsalt_backend/urls.py
@@ -30,7 +30,7 @@ from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', jwt_cookie(csrf_exempt(GraphQLView.as_view(graphiql=True)))),
+    path('graphql/', jwt_cookie(GraphQLView.as_view(graphiql=True))),
     # path('graphql/', jwt_cookie(csrf_exempt(GraphQLView.as_view(graphiql=True)))),
     # path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('csrf/', csrf_exempt(views.csrf)),
