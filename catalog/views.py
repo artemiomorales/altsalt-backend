118de33297ef9420cb04c29c7f416f0b503d5e55
Ricardo <ric.morales22@gmail.com>
2020-09-29 00:00:19 -0400

    Add code to try and set the CSRF token via HTTP

--- catalog/views.py
+++ catalog/views.py
@@ -7,7 +7,13 @@ from django.middleware.csrf import get_token
 
 
 def csrf(request):
-    return JsonResponse({'csrfToken': get_token(request)})
+    csrf_data = get_token(request)
+    response = JsonResponse({'csrfToken': csrf_data})
+    # JsonResponse({'csrfToken': get_token(request)})
+    response.set_cookie('csrftoken', csrf_data, max_age=None, expires=None, path='/',
+                        domain='altsalt-local.com', secure=True, httponly=False, samesite=None)
+    response.cookies['csrftoken']['samesite'] = 'None'
+    return response
 
 
 def ping(request):
