8043092177126b10a0cd25f4fbe48e3df680a7da
Ricardo <ric.morales22@gmail.com>
2021-01-08 13:32:26 -0500

    Change length of seo title to match title in article model

--- catalog/models/cms.py
+++ catalog/models/cms.py
@@ -30,7 +30,7 @@ class EditorialSettings(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=125)
-    seo_title = models.CharField(max_length=100, blank=True)
+    seo_title = models.CharField(max_length=125, blank=True)
     slug = models.SlugField(unique=True, blank=True)
     preview_text = models.TextField(max_length=200)
     body = models.TextField()
